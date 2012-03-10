59efe8ace18a1029e032284fb43fd686b3673037
Chris Hoge <cchoge@mac.com>
2012-03-10 09:14:38 -0800

    Walking the source tree. Basic testing.

---
+++ PhotoOrganize/PhotoOrganizer.h
@@ -0,0 +1,13 @@
+//
+//  PhotoOrganizer.h
+//  PhotoOrganize
+//
+//  Created by Chris Hoge on 3/9/12.
+//  Copyright (c) 2012 University of Oregon Neuroinformatics Center. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PhotoOrganizer : NSObject
+
+@end
---
+++ PhotoOrganize/PhotoOrganizer.m
@@ -0,0 +1,13 @@
+//
+//  PhotoOrganizer.m
+//  PhotoOrganize
+//
+//  Created by Chris Hoge on 3/9/12.
+//  Copyright (c) 2012 University of Oregon Neuroinformatics Center. All rights reserved.
+//
+
+#import "PhotoOrganizer.h"
+
+@implementation PhotoOrganizer
+
+@end
