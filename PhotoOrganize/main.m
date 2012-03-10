5e9e030b7bdc078c45d45f4970612ffddbc49343
Chris Hoge <hoge@Chriss-MacBook-Pro.local>
2012-03-09 19:58:03 -0800

    Initial Commit

---
+++ PhotoOrganize/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PhotoOrganize
+//
+//  Created by Chris Hoge on 3/9/12.
+//  Copyright (c) 2012 University of Oregon Neuroinformatics Center. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
